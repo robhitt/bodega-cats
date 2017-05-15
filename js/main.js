8c3ecd1e2cd02d9c76a1c034980f8f5f07eb8f8f
Rob Hitt <robhitt@gmail.com>
2017-05-15 12:00:27 -0400

    add separate responsive animations to cat and contact tab

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ console.log("`   \\/   ^ /");
 console.log("    | \\|| ||");
 console.log("    \\ '|| ||");
 console.log("     \\)()-())");
+
+
+if (window.innerWidth < 770) {
+  var mainCat = document.getElementById('main-cat');
+  mainCat.className += " animated zoomIn";
+}
+
+// ********** for future use **********
+// var buttons = document.getElementsByClassName('btn');
+//
+// if (window.innerWidth < 500) {
+//   console.log('less than 500')
+//   for (var i = 0; i < buttons.length; i++) {
+//     buttons[i].className += " animated bounce";
+//   }
+// }
