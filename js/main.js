77536ce92b3ce565b8dac09996c82992cd6e2f08
Rob Hitt <robhitt@gmail.com>
2017-05-15 00:33:17 -0400

    add js file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("       /\\_/\\");
+console.log("  /\\  / o o \\");
+console.log(" //\\  \\~(*)~/");
+console.log("`   \\/   ^ /");
+console.log("    | \\|| ||");
+console.log("    \\ '|| ||");
+console.log("     \\)()-())");
