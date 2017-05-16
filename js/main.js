11f177a134d21535b3a5e6ed9754f1934c0f3e28
Rob Hitt <robhitt@gmail.com>
2017-05-16 10:51:01 -0400

    minor styling and html fixes

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ console.log("     \\)()-())");
 if (window.innerWidth < 770) {
   var mainCat = document.getElementById('main-cat');
   mainCat.className += " animated zoomIn";
+
+  var buttons = document.getElementsByClassName('btn');
+    console.log('less than 500')
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].className += " animated fadeInUp";
+    }
+
+
 }
 
 // ********** for future use **********
-// var buttons = document.getElementsByClassName('btn');
-//
-// if (window.innerWidth < 500) {
-//   console.log('less than 500')
-//   for (var i = 0; i < buttons.length; i++) {
-//     buttons[i].className += " animated bounce";
-//   }
-// }
